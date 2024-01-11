dfdf65d9cd23acf832296c608eb83f38cda2c697
Anish Shrestha <anish08shrestha@gmail.com>
2024-01-11 16:00:45 +0545

    adding project card using infinite slider

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -4,7 +4,8 @@ import Featured from "../../components/featured/Featured";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
 import Slide from "../../components/slide/Slide";
 import CatCard from "../../components/catCard/CatCard";
-import { cards } from "../../data";
+import ProjectCard from "../../components/projectCard/ProjectCard";
+import { cards, projects } from "../../data";
 
 const Home = () => {
   return (
@@ -58,6 +59,43 @@ const Home = () => {
           </div>
         </div>
       </div>
+
+      <div className="features dark">
+        <div className="container">
+          <div className="item">
+            <h1>fiverr business</h1>
+            <h1>A business solution designed for teams</h1>
+            <p>
+              Upgrade to a curated experience packed with tools and benefits,
+              dedicated to business.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Connect to freelancers with proven businedd experience
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Get matched with the perfect talent by a customer success manager
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Manage teamwork and boost productivity iwth one powerful workspace
+            </div>
+            <button>Explore Fiverr business</button>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/17867764/pexels-photo-17867764/free-photo-of-deer-on-grassland.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+      <Slide sildesToShow={4} arrowsScroll={4}>
+        {projects.map((card) => (
+          <ProjectCard key={card.id} item={card} />
+        ))}
+      </Slide>
     </div>
   );
 };
