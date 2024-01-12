4bf9804ac278ce50ef07e73f697b51642ace9d2d
Anish Shrestha <anish08shrestha@gmail.com>
2024-01-12 14:50:35 +0545

    designing footer page

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -2,7 +2,108 @@ import React from "react";
 import "./Footer.scss";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="top">
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics and Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing and Translation</span>
+            <span>Video Animation</span>
+            <span>Musci and Audio</span>
+            <span>Programming and Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>SItemap</span>
+          </div>
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics and Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing and Translation</span>
+            <span>Video Animation</span>
+            <span>Musci and Audio</span>
+            <span>Programming and Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>SItemap</span>
+          </div>
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics and Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing and Translation</span>
+            <span>Video Animation</span>
+            <span>Musci and Audio</span>
+            <span>Programming and Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>SItemap</span>
+          </div>
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics and Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing and Translation</span>
+            <span>Video Animation</span>
+            <span>Musci and Audio</span>
+            <span>Programming and Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>SItemap</span>
+          </div>
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Graphics and Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing and Translation</span>
+            <span>Video Animation</span>
+            <span>Musci and Audio</span>
+            <span>Programming and Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyle</span>
+            <span>Photography</span>
+            <span>SItemap</span>
+          </div>
+        </div>
+        <hr />
+        <div className="bottom">
+          <div className="left">
+            <h2>fiverr</h2>
+            <span>Fiverr International Ltd. 2023</span>
+          </div>
+          <div className="right">
+            <div className="social">
+              <img src="img/twitter.png" alt="" />
+              <img src="img/facebook.png" alt="" />
+              <img src="img/linkedin.png" alt="" />
+              <img src="img/pinterest.png" alt="" />
+            </div>
+            <div className="link">
+              <img src="./img/language.png" alt="" />
+              <span>English</span>
+            </div>
+            <div className="link">
+              <img src="./img/coin.png" alt="" />
+              <span>USD</span>
+            </div>
+            <img src="./img/accessibility.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
