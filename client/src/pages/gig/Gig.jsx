a201c4c07c744e47f6f1ded1c97a11a47e955ab1
Anish Shrestha <anish08shrestha@gmail.com>
2024-01-18 14:52:00 +0545

    designing product page

--- client/src/pages/gig/Gig.jsx
+++ client/src/pages/gig/Gig.jsx
@@ -2,7 +2,30 @@ import React from "react";
 import "./Gig.scss";
 
 const Gig = () => {
-  return <div className="gig">Gig</div>;
+  return (
+    <div className="gig">
+      <div className="container">
+        <div className="left">
+          <span className="breadCrumbs">FIVERR {">"} GRAPHICS & DESIGN</span>
+          <h1>I will create AI generated art for you</h1>
+
+          <div className="user">
+            <img src="" alt="" />
+            <span>Will Smith</span>
+            <div className="stars">
+              <img src="./img/star.png" alt="" />
+              <img src="./img/star.png" alt="" />
+              <img src="./img/star.png" alt="" />
+              <img src="./img/star.png" alt="" />
+              <img src="./img/star.png" alt="" />
+              <span>5</span>
+            </div>
+          </div>
+        </div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Gig;
